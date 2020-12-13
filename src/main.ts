a4fcf9d2fc9443df6c4f632d426d01fb3cb185e4
Ivan Reshetnyak <ivan.reshetnyak@gmail.com>
2020-12-13 21:22:59 +0300

    Add console parsing

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,25 @@
-import { Formation } from "./Formation";
-import { State } from "./Cell";
 import { FormationOcta } from "./formations/FormationOcta";
 
-let formation: Formation = new FormationOcta(10, 10);
-//formation.randomize();
-formation.read("in/1.txt");
-/*
-let formation: FormationOcta = new FormationOcta(4, 5);
-formation.set(1, -1, State.Alive);
-formation.set(1, 0, State.Alive);
-formation.set(1, 1, State.Alive);
-*/
+let formation: FormationOcta = new FormationOcta(0, 0);
 
-setInterval(
-  function(): void {
-    console.log(formation.render());
-    formation.update();
-  }, 500);
+let parsed: boolean = false;
+
+if (process.argv[2] == "random") {
+  let sizeX = parseInt(process.argv[3]);
+  let sizeY = parseInt(process.argv[3]);
+  formation.resize(sizeX, sizeY);
+  formation.randomize();
+  parsed = true;
+} else if (process.argv[2] == "file") {
+  formation.read(process.argv[3]);
+  parsed = true;
+}
+
+if (parsed)
+  setInterval(
+    function(): void {
+      console.log(formation.render());
+      formation.update();
+    }, 1000);
+else
+  console.log("Incorrect usage: expected 'npm start random <sizeX> <sizeY>' or 'npm start file <filename>'!");
